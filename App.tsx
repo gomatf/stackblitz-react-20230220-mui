524771e9b7e0901c76050b28b6627254aeef0c06
gomatf <125886829+gomatf@users.noreply.github.com>
2023-02-20 22:29:34 +0900

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import './style.css';
+import { Button } from '@mui/material';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+
+      <Button variant="contained">Sample Button</Button>
     </div>
   );
 }
